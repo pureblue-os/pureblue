eed8132b897d4045a2f35728855ae61b4291f975
Shiba <44804845+DeepDoge@users.noreply.github.com>
2025-03-11 00:26:53 +0300

    forgot the repo

--- build/asus/setup.sh
+++ build/asus/setup.sh
@@ -6,6 +6,8 @@ echo "Setup Asus"
 set -x
 set -euxo pipefail
 
+wget https://copr.fedorainfracloud.org/coprs/lukenukem/asus-linux/repo/fedora-${FEDORA_VERSION}/lukenukem-asus-linux-fedora-${FEDORA_VERSION}.repo -O /etc/yum.repos.d/_copr_lukenukem-asus-linux.repo
+
 # asusctl doesnt support with tuned-ppd at this time.
 rpm-ostree override remove tuned-ppd --install power-profiles-daemon
 
